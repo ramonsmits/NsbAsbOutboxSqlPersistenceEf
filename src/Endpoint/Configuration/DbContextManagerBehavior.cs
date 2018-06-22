50e2455be4c16a203ca0c8d98fdd209028946a89
Ramon Smits <ramon.smits@gmail.com>
2018-06-22 15:32:16 +0200

    DbContext is now lazy, which means it is only created if actually used in a handler.

--- src/Endpoint/Configuration/DbContextManagerBehavior.cs
+++ src/Endpoint/Configuration/DbContextManagerBehavior.cs
@@ -4,24 +4,38 @@ using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Pipeline;
 
-public class DbContextManagerBehavior<TDbContext> : Behavior<IInvokeHandlerContext> where TDbContext : DbContext
+public class DbContextManagerBehavior<T> : Behavior<IInvokeHandlerContext> where T : DbContext
 {
     public override async Task Invoke(IInvokeHandlerContext context, Func<Task> next)
     {
-        var session = context.SynchronizedStorageSession;
-        var sqlPersistenceSession = session.SqlPersistenceSession();
-        var dbContext = (TDbContext)Activator.CreateInstance(typeof(TDbContext), sqlPersistenceSession.Connection);
-
-        using (dbContext)
+        var lazyContext = new Lazy<T>(() =>
         {
+            var session = context.SynchronizedStorageSession;
+            var sqlPersistenceSession = session.SqlPersistenceSession();
+            var dbContext = (T)Activator.CreateInstance(typeof(T), sqlPersistenceSession.Connection);
             dbContext.Database.UseTransaction(sqlPersistenceSession.Transaction);
-            context.Extensions.Set(dbContext);
+            return dbContext;
+        });
 
+        context.Extensions.Set(lazyContext);
+
+        try
+        {
             await next()
                 .ConfigureAwait(false);
 
-            await dbContext.SaveChangesAsync()
-                .ConfigureAwait(false);
+            if (lazyContext.IsValueCreated)
+            {
+                await lazyContext.Value.SaveChangesAsync()
+                    .ConfigureAwait(false);
+            }
+        }
+        finally
+        {
+            if (lazyContext.IsValueCreated)
+            {
+                lazyContext.Value.Dispose();
+            }
         }
     }
 }
