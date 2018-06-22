fb3468f8d62856b5d5cc71f7f163fdc90c683c49
Ramon Smits <ramon.smits@gmail.com>
2018-06-22 14:53:16 +0200

    Rename

---
+++ src/Endpoint/OrderStorage/OrderStorageContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NServiceBus;
+
+/// <summary>
+/// This only exist to wrap the extension method as these cannot easily to mocked for unit testing.
+/// </summary>
+class OrderStorageContext : IOrderStorageContext
+{
+    public OrderDbContext Get(IMessageHandlerContext context)
+    {
+        if (context.Extensions.TryGet(out OrderDbContext dataContext))
+        {
+            return dataContext;
+        }
+        throw new Exception($"No DbContext set for '{typeof(OrderDbContext)}.");
+    }
+}
